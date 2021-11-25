ff37f192078cfc4a3b3dafad6dc9839edc2fa065
Daniel Lincoln <dlincol1@yahoo.com>
2021-11-24 20:04:27 -0500

    Create VRED-ImportSounds.py

---
+++ VRED-ImportSounds.py
@@ -0,0 +1,24 @@
+# Import Sound (.wav) files into a Group node
+
+import os
+
+audio_dir = "C:/VRED_Audio/"
+
+scene_parent = createNode('Group', 'Sounds', True)
+
+def importSound(filename):
+    full_path = audio_dir + filename
+
+    sound_node = createNode("Sound", filename, scene_parent)
+    sound_node.setFieldString("soundFile", full_path)
+    sound_node.setFieldBool("play", False)
+    sound_node.setFieldBool("loop", False)
+
+    return(sound_node)
+    
+for file in os.listdir(audio_dir):
+    if file.endswith('.wav'):
+        node = importSound(file)
+        print(f'Imported: {node.getName()}')
+
+updateScenegraph(True)
