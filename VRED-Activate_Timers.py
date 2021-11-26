dfc392f35479afbe8493abd6394fef9a2fac008f
Daniel Lincoln <dlincol1@yahoo.com>
2021-11-26 08:08:47 -0500

    Create VRED-Activate_Timers.py

---
+++ VRED-Activate_Timers.py
@@ -0,0 +1,8 @@
+index = 0 if node_ptrs[0].getActive() else 1
+       
+try:
+    sound_ptrs[index].setFieldBool("play", True)
+except:
+    print('\nSound list not configured.')
+    
+for i in timer_lists[index]: i.setActive(True)
