2314350973929a058aac70ee56fed394ae114107
Daniel Lincoln <dlincol1@yahoo.com>
2021-11-26 08:08:06 -0500

    Delete VRED-ActivateNodeTimers.py

--- VRED-ActivateNodeTimers.py
+++
@@ -1,9 +0,0 @@
-index = 0 if node_ptrs[0].getActive() else 1
-       
-try:
-    sound_ptrs[index].setFieldBool("play", True)
-    
-except:
-    print('\nSound list not configured.')
-    
-for i in timer_lists[index]: i.setActive(True)
